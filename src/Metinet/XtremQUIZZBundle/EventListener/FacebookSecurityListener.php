bdcaa8ba4ddd8fbf60b6f5f28d463d965da03714
Haag Olivier <fury473@gmail.com>
2013-05-12 16:47:53 +0200

    FacebookListener et FbUserManager ajoutés, index commenté

--- src/Metinet/XtremQUIZZBundle/EventListener/FacebookSecurityListener.php
+++ src/Metinet/XtremQUIZZBundle/EventListener/FacebookSecurityListener.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Metinet\Bundle\XtremQUIZZBundle\EventListener;
+namespace Metinet\XtremQUIZZBundle\EventListener;
 
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
