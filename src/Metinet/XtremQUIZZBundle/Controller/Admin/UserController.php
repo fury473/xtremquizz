8c3c208e084b6ccfebf29be6c0e18fe05a432060
Mamori <mohamedthelji.majri@gmail.com>
2013-05-13 16:24:54 +0200

    Connexion BO

--- src/Metinet/XtremQUIZZBundle/Controller/Admin/UserController.php
+++ src/Metinet/XtremQUIZZBundle/Controller/Admin/UserController.php
@@ -17,4 +17,45 @@ class UserController extends Controller
         
         return array();
     }
+    
+    /**
+     * Action de login
+     * 
+     * @Route("/admin/connexion", name="connexion")
+     * @Template()
+     */
+    public function loginAction()
+    {
+        $request = $this->getRequest();
+        $session = $request->getSession();
+        
+        // On vérifie l’identification
+        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+        } else {
+            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+        }
+        
+        return array('error' => $error);
+    }
+    
+     /**
+     * Action de check login
+     *
+     * @Route("/admin/login_check", name="login_check")
+     */
+    public function loginCheckAction() 
+    {
+        
+    }
+    
+     /**
+     * Action de logout
+     * @Route("/admin/deconnexion", name="deconnexion")
+     *
+     */
+    public function logoutAction()
+    {
+    
+    }
 }
